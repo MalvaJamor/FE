beeef677bc0a8c52de4964e872447c190397d6c9
Taiwo Akerele <davcodes24@gmail.com>
2024-06-20 05:05:51 -0700

    link forgotten password to the password page

--- src/components/Auth/ParagraphAccess.jsx
+++ src/components/Auth/ParagraphAccess.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function ParagraphAccess() {
   return (
     <div className="flex items-center justify-between mt-4">
@@ -11,7 +13,9 @@ export default function ParagraphAccess() {
         <label htmlFor="remember" className="font-moreLight text-custom-purplish-blue">Remember me</label>
       </div>
       <div>
+        <Link to={'/password'} >
         <p className="font-moreLight underline text-custom-purplish-blue">Forgot password?</p>
+        </Link>
       </div>
     </div>
   );
