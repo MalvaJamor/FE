175d559eaaad71b3dd20d2dd57cc962f08f15e08
Taiwo Akerele <davcodes24@gmail.com>
2024-06-20 03:55:27 -0700

    getting-updated version

--- src/components/Auth/ShowPassword.jsx
+++ src/components/Auth/ShowPassword.jsx
@@ -3,7 +3,7 @@ import hide from '../assets/eye-slash.svg';
 export default function ShowPassword({isVisible, divClass}) {
   return (
     <div className={divClass}>
-        <img src={hide} alt="hide" className={isVisible} />
+        <img src={hide} alt="hides" className={isVisible} />
     </div>
   )
 }
